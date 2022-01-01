300fbbc10d5734bcf2b38b45be9ac542ec179516
lijing00333 <bachi@taobao.com>
2022-01-01 20:20:46 +0800

    Update home.js

--- example/home.js
+++ example/home.js
@@ -8,7 +8,7 @@ const fh = new FileHandler();
 (function(){
 
   var rec = new Recorder({
-    url: 'rtsp://admin:hello1234@192.168.0.112:554/h264/ch1/sub/av_stream',
+    url: 'rtsp://admin:123456@192.168.0.1:554/h264/ch1/sub/av_stream',
     timeLimit: 60 * 15, // 15 minutes
     folderSizeLimit : 5,
     folder: join(__dirname , '/videos/'),
