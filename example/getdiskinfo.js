126aed6a26428fc3e001af44e886cdd9ab24b696
拔赤 <bachi@taobao.com>
2024-09-25 11:37:41 +0800

    新增磁盘空间的检测，只加上了 require ，逻辑还没加

---
+++ example/getdiskinfo.js
@@ -0,0 +1,25 @@
+
+
+const checkDiskSpace = require('check-disk-space').default;
+
+// On Linux or macOS
+// checkDiskSpace(__dirname).then((diskSpace) => {
+//   console.log(diskSpace)
+//   // {
+//   //     diskPath: '/',
+//   //     free: 12345678,
+//   //     size: 98756432
+//   // }
+//   // Note: `free` and `size` are in bytes
+// })
+
+
+const hasEnoughDiskSpace = async (pathName) => {
+  let freeSize = await checkDiskSpace(pathName);
+  if(freeSize.free < 70000000) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
